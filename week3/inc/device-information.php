ed3adbc587cdb1e88c9a5622228c3dc12dca5267
Johan Kies <johank93@gmail.com>
2015-09-29 17:17:16 +0200

    - Add: MySQLi in file - Add: Device information page

---
+++ week3/inc/device-information.php
@@ -0,0 +1,50 @@
+<?php
+
+require('connection.php');
+
+if (isset($_GET['device']) && isset($_GET['comparison_type']) && isset($_GET['region_type'])) {
+   
+    // SELECT fields
+    $sql = "SELECT datum AS 'datum', tijd AS 'tijd', AVG(waarde) AS 'gemiddeldewaarde' ";
+    
+    // SELECT tables
+    $sql .= " FROM meting "
+            . "INNER JOIN apparaat_huishouden ON meting.app_hh = apparaat_huishouden.id "
+            . "INNER JOIN apparaat ON apparaat_huishouden.apparaat_fk = apparaat.id";
+
+    // device number of the household
+    $device = $_GET['device'];
+    
+    $comparison_type = $_GET['comparison_type'];  
+    if ($comparison_type == "1") {
+        // same device type number
+        $sql .= " WHERE typenummer = '" . $device . "'";
+    }
+    else if ($comparison_type == "2") {
+        // same device kind
+    }
+
+    $region_type = $_GET['region_type'];
+    if ($region_type == "1") {
+        // search on zipcode
+    }
+    else if ($region_type == "2") {
+        // search on county
+    }
+    
+    // GROUP BY
+    $sql .= " GROUP BY datum,tijd";
+    
+    // ORDER AND SELECTION
+    $sql .= " ORDER BY datum DESC, tijd DESC";
+    $sql .= " LIMIT 0,10";
+    
+    $result = $mysqli->query($sql) or die($mysqli->error);
+    
+    $data = $result->fetch_all( MYSQLI_ASSOC );
+    echo json_encode( $data );
+}
+else {
+    // return empty string
+    echo "";
+}
