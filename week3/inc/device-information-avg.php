8bb580c33f9fb4a6dfc644cf79acc26a1be5b907
Johan Kies <johank93@gmail.com>
2015-10-02 19:10:49 +0200

    - Add: Graphic view

--- week3/inc/device-information-avg.php
+++ week3/inc/device-information-avg.php
@@ -25,12 +25,28 @@ if (isset($_GET['device_id']) && isset($_GET['comparison_type']) && isset($_GET[
     else if ($comparison_type == "2") {
         // same device kind
     }
+    
+    $sql2 = "SELECT huishouden.id,huishouden.postcode,huisnummer,grootte,telefoonnummer,email,street,city,province "
+            . "FROM huishouden "
+            . "LEFT JOIN postcode on huishouden.postcode = postcode.postcode "
+            . "WHERE huishouden.id = " . $_SESSION['huishouden_id'];
 
+    $result2 = $mysqli->query($sql2) or die($mysqli->error);
+    $data2 = $result2->fetch_row();
+    
+    if ($data2[8] == "")
+        $data2[8] = "Groningen";
+   
     $region_type = $_GET['region_type'];
     if ($region_type == "1") {
         // search on zipcode
         // TODO: Replace with current household zipcode
-        $sql .= " AND huishouden.id = " . $_SESSION['huishouden_id'];
+        $sql .= " AND huishouden.postcode = '" . $data2[1] . "'";
+    }
+    else if ($region_type == "2") {
+        // search on county
+        // TODO: Replace with current household county
+        $sql .= " AND postcode.province = '" . $data2[8] . "'";
     }
     
     // GROUP BY
@@ -39,6 +55,7 @@ if (isset($_GET['device_id']) && isset($_GET['comparison_type']) && isset($_GET[
     // ORDER AND SELECTION
     $sql .= " ORDER BY datum DESC, tijd DESC";
     $sql .= " LIMIT 0,10";
+    
     
     $result = $mysqli->query($sql) or die($mysqli->error);
     
--- week3/pages/apparaat_meting.php
+++ week3/pages/apparaat_meting.php
@@ -117,6 +117,7 @@ if (isset($_GET['id'])) {
         }
         ?>
 
+<div class="col-md-6">
         <form class="form-inline" method="POST">
             <input type="text" class="form-control" name="day" size="1px" value="<?php echo $dayselect; ?>">
             <input type="text" class="form-control" name="month" size="1px" value="<?php echo $monthselect; ?>">
@@ -173,6 +174,148 @@ if (isset($_GET['id'])) {
                     </div>
                 </div>
             </form>
+        </div>
+<div class="col-md-6">
+    
+    <div class="form-horizontal" method="POST">
+    <legend>Instellingen grafiek</legend>
+    <div class="form-group">
+        <label for="inputEmail3" class="col-sm-4 control-label">Regio</label>
+        <div class="col-sm-8">
+            <select name="inputRegio" id="inputRegio"  class="form-control">
+                <option value="1">Eigen postcode</option>
+                <option value="2">Gehele provincie</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputEmail3" class="col-sm-4 control-label">Vergelijken met</label>
+        <div class="col-sm-8">
+            <select name="inputComparison" id="inputComparison"  class="form-control">
+                <option value="1">Eigen typenummer</option>
+                <option value="2">Zelfde soort apparaten</option>
+            </select>
+        </div>
+    </div>    
+</div>
+    <canvas id="myChart" class="col-md-12"></canvas>
+</div>
+<script>
+    $( document ).ready(function() {
+        $( "#inputRegio" ).change(function(e) {
+            GetGraphic($( "#inputComparison" ).val(),$( "#inputRegio" ).val());
+        });
+        
+        $( "#inputComparison" ).change(function(e) {
+            GetGraphic($( "#inputComparison" ).val(),$( "#inputRegio" ).val());
+        });
+        
+        GetGraphic($( "#inputComparison" ).val(),$( "#inputRegio" ).val());
+    });
+    
+    
+    
+    
+    function GetGraphic(comparison_type,region_type) {
+    
+        $.get("/inc/device-information.php?device_id=" + <?php echo $_GET['id'] ?> + "&comparison_type="+comparison_type+"&region_type="+region_type, function(data) {
+
+            var json = JSON.decode(data);
+
+            var labels = [];
+            //var valuesAVG = [];
+            var valuesMine = [];
+
+            for (var i = 0; i < json.length; i++) {
+                var obj = json[i];
+                labels[i] = obj.tijd.substring(0, 5);
+                //valuesAVG[i] = parseFloat(obj.gemiddeldewaarde);
+                valuesMine[i] = parseFloat(obj.gemiddeldewaarde);
+            }
+            labels.reverse();
+            //valuesAVG.reverse();
+            valuesMine.reverse();
+
+            $.get("/inc/device-information-avg.php?device_id=" + <?php echo $_GET['id'] ?> + "&comparison_type="+comparison_type+"&region_type="+region_type, function(data) {
+
+                var json = JSON.decode(data);
+
+                var valuesAVG = [];
+
+                for (var i = 0; i < json.length; i++) {
+                    var obj = json[i];
+                    valuesAVG[i] = parseFloat(obj.gemiddeldewaarde);
+                }
+                valuesAVG.reverse();
+
+                var ctx = document.getElementById("myChart").getContext("2d");
+                var options = {
+                    scaleOverride : true,
+                    scaleSteps : 5,
+                    scaleStepWidth : 5,
+                    scaleStartValue : 0 ,
+                    ///Boolean - Whether grid lines are shown across the chart
+                    scaleShowGridLines: true,
+                    //String - Colour of the grid lines
+                    scaleGridLineColor: "rgba(0,0,0,.05)",
+                    //Number - Width of the grid lines
+                    scaleGridLineWidth: 1,
+                    //Boolean - Whether to show horizontal lines (except X axis)
+                    scaleShowHorizontalLines: true,
+                    //Boolean - Whether to show vertical lines (except Y axis)
+                    scaleShowVerticalLines: true,
+                    //Boolean - Whether the line is curved between points
+                    bezierCurve: true,
+                    //Number - Tension of the bezier curve between points
+                    bezierCurveTension: 0.4,
+                    //Boolean - Whether to show a dot for each point
+                    pointDot: true,
+                    //Number - Radius of each point dot in pixels
+                    pointDotRadius: 4,
+                    //Number - Pixel width of point dot stroke
+                    pointDotStrokeWidth: 1,
+                    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                    pointHitDetectionRadius: 20,
+                    //Boolean - Whether to show a stroke for datasets
+                    datasetStroke: true,
+                    //Number - Pixel width of dataset stroke
+                    datasetStrokeWidth: 2,
+                    //Boolean - Whether to fill the dataset with a colour
+                    datasetFill: true,
+                    //String - A legend template
+                    multiTooltipTemplate: "<%= value %> - <%= datasetLabel %>"
+                };
+
+                var chartdata = {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: "Mijn apparaat",
+                            fillColor: "rgba(220,220,220,0.2)",
+                            strokeColor: "rgba(220,220,220,1)",
+                            pointColor: "rgba(220,220,220,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: valuesMine
+                        },
+                        {
+                            label: "Prov. gem.",
+                            fillColor: "rgba(151,187,205,0.2)",
+                            strokeColor: "rgba(151,187,205,1)",
+                            pointColor: "rgba(151,187,205,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(151,187,205,1)",
+                            data: valuesAVG
+                        }
+                    ]
+                };
+                new Chart(ctx).Line(chartdata, options);
+            });
+        });
+    }
+</script>
         <?php
         }else{
             echo "Onjuiste datum!";
